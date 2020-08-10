6ec8ac4b69ca5fa5ca4d41b8672057dd8b0c55f7
Andrew Mak <makandre@ca.ibm.com>
2020-08-10 11:34:23 -0400

    update
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>

--- actions/register_org.py
+++ actions/register_org.py
@@ -2,6 +2,7 @@ from lib.base import BaseGithubAction
 import json
 from st2client.client import Client
 from st2client.models import KeyValuePair
+from lib.formatters import filter_orgs
 
 __all__ = [
     'AddOrgAction'
@@ -27,10 +28,18 @@ class AddOrgAction(BaseGithubAction):
             url = DEFAULT_API_URL
             github_type = 'online'
 
-        org = {'user': user, 'url': url, 'token': token, 'type': github_type, 'repositories': repositories, 'event_type_whitelist': event_type_whitelist}
-        dict[user+'|'+url]=org
+        key = user + '|' + url
+        org = dict.get(key)
+
+        if org == None:
+            org = {'user': user, 'url': url, 'token': token, 'type': github_type, 'repositories': repositories, 'event_type_whitelist': event_type_whitelist}
+        else:
+            org['repositories'] = list(set(org['repositories']).union(repositories))
+            org['event_type_whitelist'] = list(set(org['event_type_whitelist']).union(event_type_whitelist))
+
+        dict[key]=org
         gitorgs=json.dumps(dict)
 
         client.keys.update(KeyValuePair(name='git-orgs', value=gitorgs, secret=True))
 
-        return list(dict.keys())
+        return filter_orgs(dict)
