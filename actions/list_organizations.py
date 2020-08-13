489b3c96e66fb4315a4afd7d916d94e597c5e8c8
Steven Hung <sghung@ca.ibm.com>
2020-08-13 11:47:47 -0400

    add files

--- actions/list_organizations.py
+++ actions/list_organizations.py
@@ -20,7 +20,7 @@ class ListOrganizationsAction(BaseGithubAction):
         if since:
             kwargs['since'] = datetime.datetime.fromtimestamp(since)
 
-        organizations = user.get_organizations(**kwargs)
+        organizations = user.get_orgs(**kwargs)
         organizations = list(organizations)
 
         result = []
