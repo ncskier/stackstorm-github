d7a0cffefbe065941bae6f6d19405091fb9df5e4
Akihiko (Aki) Kuroda <16141898+akihikokuroda@users.noreply.github.com>
2020-08-13 16:40:19 -0400

    Add list_orgs (#18)
    * add list_orgs

---
+++ actions/list_orgs.py
@@ -0,0 +1,32 @@
+import datetime
+
+from lib.base import BaseGithubAction
+from lib.formatters import org_to_dict
+
+__all__ = [
+    'ListOrgsAction'
+]
+
+
+class ListOrgsAction(BaseGithubAction):
+    def run(self, user, base_url, limit=20):
+        if base_url == None:
+            self._reset(user)
+        else:
+            self._reset(user+'|'+base_url)
+        user = self._client.get_user(user)
+
+        kwargs = {}
+
+        orgs = user.get_orgs(**kwargs)
+        orgs = list(orgs)
+
+        result = []
+        for index, org in enumerate(orgs):
+            repo = org_to_dict(org=org)
+            result.append(repo)
+
+            if (index + 1) >= limit:
+                break
+
+        return result
