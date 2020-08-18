de9638191ed5e6cd2903fcda1eea52e5b1b07258
akihikokuroda <akuroda@us.ibm.com>
2020-08-18 07:24:57 -0700

    review comments

--- actions/lib/base.py
+++ actions/lib/base.py
@@ -63,6 +63,8 @@ class BaseGithubAction(Action):
             self._client = Github(self.token, base_url=self.base_url)
 
     def _temp_client(self, token, base_url):
+        if base_url == None:
+            base_url = self.config.get('base_url', None)
         return Github(token, token, base_url=base_url)
 
     def _web_session(self, web_url=DEFAULT_WEB_URL):
